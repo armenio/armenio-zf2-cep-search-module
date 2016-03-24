4dadb1354a7595d4b0afc9461f2d1e414e73a572
Rafael Armenio <rafael.armenio@gmail.com>
2016-03-24 16:29:18 -0300

    Added files via upload

---
+++ src/Cep.php
@@ -0,0 +1,80 @@
+<?php
+namespace Aircode\Cep;
+
+use Zend\Http\Client;
+use Zend\Http\Client\Adapter\Curl;
+use Zend\Json\Json;
+
+/**
+ * @author	 Rafael Armenio
+ */
+class Cep
+{
+	public static function search($cep = '00000000')
+	{
+		$result = array(
+			'endereco' => '',
+			'bairro' => '',
+			'cep' => $cep,
+			'cidade' => '',
+			'estado' => '',
+			'pais' => 'Brasil'
+		);
+
+		$cep = preg_replace('/[^0-9]*/', '', $cep);
+
+		try{
+			$uri = sprintf('http://aircode.com.br/webservice/correios/cep/%s', $cep);
+			$client = new Client($uri);
+			$client->setAdapter(new Curl());
+			$client->setMethod('GET');
+			$client->setOptions(array(
+				'curloptions' => array(
+					CURLOPT_HEADER => false,
+				)
+			));
+
+			
+			$response = $client->send();
+			
+				
+			$body = $response->getBody();
+			
+			$json = Json::decode($body, 1);
+			//{"id":"22881","cidade":"Curitiba","logradouro":"Marechal Deodoro","bairro":"Centro","cep":"80060-010","tp_logradouro":"Rua","uf":"PR"}
+
+			if( ! empty($json['cep']) ){
+				$result = array(
+					'endereco' => trim(sprintf('%s %s', $json['tp_logradouro'], $json['logradouro'])),
+					'bairro' => $json['bairro'],
+					'cep' => $json['cep'],
+					'cidade' => $json['cidade'],
+					'estado' => $json['uf'],
+					'pais' => $json['pais']
+				);
+			}else{
+				$result = $json;
+			}
+
+            $isException = false;
+		} catch (\Zend\Http\Exception\RuntimeException $e){
+            $isException = true;
+        } catch (\Zend\Http\Client\Adapter\Exception\RuntimeException $e){
+        	$isException = true;
+        } catch (\Zend\Json\Exception\RuntimeException $e) {
+			$isException = true;
+		} catch (\Zend\Json\Exception\RecursionException $e2) {
+			$isException = true;
+		} catch (\Zend\Json\Exception\InvalidArgumentException $e3) {
+			$isException = true;
+		} catch (\Zend\Json\Exception\BadMethodCallException $e4) {
+			$isException = true;
+		}
+
+		if( $isException === true ){
+			//código em caso de problemas no decode
+		}
+
+		return $result;
+	}
+}
